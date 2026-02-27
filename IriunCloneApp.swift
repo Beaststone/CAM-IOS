7ebabb4f17d492345ccf206d2f6537bbaa2bb5b5
Beaststone <61944481+Beaststone@users.noreply.github.com>
2026-02-27 18:25:24 +0100

    Update IriunCloneApp.swift

--- IriunCloneApp.swift
+++ IriunCloneApp.swift
@@ -2,13 +2,9 @@ import SwiftUI
 
 @main
 struct IriunCloneApp: App {
-    @StateObject private var appState = AppState()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(appState)
         }
     }
 }
-
